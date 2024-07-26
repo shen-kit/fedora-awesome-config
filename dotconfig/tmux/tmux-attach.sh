315a690b2bfb45a3be4152f4faec635afa3a442e
Shen-Kit Hia <shenkit108+1@gmail.com>
2024-07-26 13:52:03 +1000

    updates tmux attach
    - only start with main, no obsidian vaults by default

--- dotconfig/tmux/tmux-attach.sh
+++ dotconfig/tmux/tmux-attach.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 # create tmux sessions if not exists
-sessionNames=("main" "personal-notes")
-startCmds=("cd ~ && clear" "cd ~/gDrive/1-personal-notes && clear && v Dashboard.md")
+sessionNames=("main")
+startCmds=("cd ~ && clear")
+# sessionNames=("main" "personal-notes")
+# startCmds=("cd ~ && clear" "cd ~/gDrive/1-personal-notes && clear && v Dashboard.md")
 # sessionNames=("main" "personal-notes" "zettelkasten")
 # startCmds=("cd ~ && clear" "cd ~/gDrive/1-personal-notes && clear && v Dashboard.md" "cd ~/gDrive/2-zettelkasten && clear && v .")
 
