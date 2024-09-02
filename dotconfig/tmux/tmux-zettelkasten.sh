5ccab0eff725c6b3681ffdea9161399817ea7ff7
Shen-Kit Hia <shenkit108+1@gmail.com>
2024-09-02 17:36:35 +1000

    update tmux scripts to fix bug when session already exists

--- dotconfig/tmux/tmux-zettelkasten.sh
+++ dotconfig/tmux/tmux-zettelkasten.sh
@@ -4,6 +4,6 @@
 # create the session if it does not exist
 
 tmux has-session -t zettelkasten ||
-    tmux new -s zettelkasten -d && tmux send-keys "cd ~/gDrive/2-zettelkasten; nvim _inbox.md" Enter;
+    (tmux new -s zettelkasten -d && tmux send-keys "cd ~/gDrive/2-zettelkasten; nvim _inbox.md" Enter)
 
 tmux switch-client -t zettelkasten
