e9aebeb9477463d6f6a518482feb425d4ccb001e
Shen-Kit Hia <shenkit108+1@gmail.com>
2024-10-20 15:39:39 +1100

    TMUX update session scripts after merging obsidian vaults

--- dotconfig/tmux/tmux-zettelkasten.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# switch to a 'zettelkasten' session with my zettelkasten open in neovim
-# create the session if it does not exist
-
-tmux has-session -t zettelkasten ||
-    (tmux new -s zettelkasten -d && tmux send-keys "cd ~/gDrive/2-zettelkasten; nvim _inbox.md" Enter)
-
-tmux switch-client -t zettelkasten
