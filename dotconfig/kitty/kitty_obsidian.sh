1ed6a2bbe4a256625a0ef80850e70e4f00c06452
Shen-Kit Hia <shenkit108+1@gmail.com>
2024-11-02 13:59:40 +1100

    KITTY adds custom kitten for obsidian and removes unused scripts

--- dotconfig/kitty/kitty_obsidian.sh
+++
@@ -1,6 +0,0 @@
-kitty @ ls | grep '"title": "obsidian"'
-if [[ $? -ne 0 ]]; then
-  kitty @ launch --type=tab --tab-title=obsidian --cwd=~/gDrive/1-obsidian/ nvim
-else
-  kitty @ focus-tab --match title:obsidian
-fi
