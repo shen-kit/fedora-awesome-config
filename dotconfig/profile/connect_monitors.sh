a8ebf9b75952faa64cb7a3c2321890e1b14a81c6
Shen-Kit Hia <shenkit108+1@gmail.com>
2025-01-22 10:05:56 +1100

    PROFILE updates connect_monitors script to work with monitors connected to either side

--- dotconfig/profile/connect_monitors.sh
+++ dotconfig/profile/connect_monitors.sh
@@ -5,18 +5,15 @@ pkill conky
 
 xrandr --output eDP-1 --mode 1280x720 --output DP-1 --off --output DP-2 --off --output DP-3 --off
 
-# single monitor setup
+# left monitor setup
 if [[ ! -z $(xrandr | grep --invert-match "eDP-1" | grep "DP-1 connected") ]] then # connected
    xrandr --output DP-1 --mode 1920x1080 --primary --left-of eDP-1 #--output eDP-1 --mode 1280x720 --right-of DP-1
 fi
 
-# double monitor setup
-# if [[ ! -z $(xrandr | grep "DP-2 connected" | grep --invert-match "eDP-1") ]] then # connected
-#     xrandr --output DP-2 --mode 1920x1080 --primary --output DP-1 --mode 1920x1080 --right-of DP-2 ||
-#         xrandr --output DP-2 --mode 1920x1080 --primary   
-# else # disconnected
-#     xrandr --output eDP-1 --mode 1280x720
-# fi
+# right single-monitor setup
+if [[ ! -z $(xrandr | grep --invert-match "eDP-1" | grep "DP-3 connected") ]] then # connected
+   xrandr --output DP-3 --mode 1920x1080 --primary --right-of eDP-1 
+fi
 
 # restart conky
 # conky -c ~/.config/conky/conky.conf &
